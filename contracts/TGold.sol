5be696a3199e2fd535b21e6447ab702bebfabe4b
longhg <longhg1988@gmail.com>
2021-11-26 21:31:22 +0700

    Update erc20 tokens

--- contracts/TGold.sol
+++ contracts/TGold.sol
@@ -1,12 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract TGold is ERC20PresetFixedSupply {
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-    constructor(string memory name, string memory symbol, uint256 initialSupply)
-        ERC20PresetFixedSupply(name, symbol, initialSupply, _msgSender())
-    {}
+contract TGold is AccessControlEnumerable, ERC20Burnable {
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
+    {
+        address msgSender = _msgSender();
+
+        _setupRole(DEFAULT_ADMIN_ROLE, msgSender);
+
+        _setupRole(MINTER_ROLE, msgSender);
+    }
+
+    function mint(address to, uint256 amount)
+        public
+    {
+        require(hasRole(MINTER_ROLE, _msgSender()), "TGold: must have minter role to mint");
+
+        _mint(to, amount);
+    }
 
 }
--- contracts/WBond.sol
+++ contracts/WBond.sol
@@ -1,12 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WBond is ERC20PresetFixedSupply {
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+interface IBPContract {
+
+    function protect(address sender, address receiver, uint256 amount) external;
+
+}
+
+contract WBond is Ownable, ERC20Burnable {
+
+    IBPContract public bpContract;
+
+    bool public bpEnabled;
+    bool public bpDisabledForever;
 
     constructor(string memory name, string memory symbol, uint256 initialSupply)
-        ERC20PresetFixedSupply(name, symbol, initialSupply, _msgSender())
-    {}
+        ERC20(name, symbol)
+    {
+        _mint(_msgSender(), initialSupply);
+    }
+
+    function setBPContract(address addr)
+        public
+        onlyOwner
+    {
+        require(address(bpContract) == address(0), "WBond: can only be initialized once");
+
+        bpContract = IBPContract(addr);
+    }
+
+    function setBPEnabled(bool enabled)
+        public
+        onlyOwner
+    {
+        bpEnabled = enabled;
+    }
+
+    function setBPDisableForever()
+        public
+        onlyOwner
+    {
+        require(!bpDisabledForever, "WBond: bot protection disabled");
+
+        bpDisabledForever = true;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override
+    {
+        if (bpEnabled && !bpDisabledForever) {
+            bpContract.protect(from, to, amount);
+        }
+    }
 
 }
