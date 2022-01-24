692bae8828e4c6fb08fe955038eccda70456ddd0
Thai Huy <likenew@likes-MacBook-Pro.local>
2022-01-25 00:53:00 +0700

    clain reward TGlod

---
+++ contracts/RewardManagement.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./libs/Signature.sol";
+
+interface ITGlod {
+    function mint(address to, uint256 amount) external;
+}
+
+contract RewardManagement is AccessControlEnumerable {
+    using SafeMath for uint256;
+    using SafeMath for uint32;
+    using Signature for bytes32;
+
+    event ClaimReward(address, uint256, string);
+
+    uint256 public constant SECOND_PER_DATE = 86400;
+    bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+
+    ITGlod tglod;
+    uint256 private _expiredTime = 300;
+    mapping(address => mapping(uint32 => uint256)) private _userQuota;
+    mapping(uint32 => uint256) private _dateQuota;
+    mapping(string => bool) private _claimId;
+    uint256 public quotaMintPerDate;
+    uint256 public quotaUserMintPerDate;
+    bool public verifyQuota;
+
+    modifier onlyAdmin() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "RewardManagement: Must Have Admin Role"
+        );
+        _;
+    }
+
+    modifier onlySigner() {
+        require(
+            hasRole(SIGNER_ROLE, _msgSender()),
+            "RewardManagement: Must Have Signer Role"
+        );
+        _;
+    }
+
+    modifier notNull(address _address) {
+        require(_address != address(0), "RewardManagement: Address invalid");
+        _;
+    }
+
+    constructor(
+        ITGlod _tglod,
+        uint256 _quotaMintPerDate,
+        uint256 _quotaUserMintPerDate,
+        bool _verifyQuota
+    ) {
+        tglod = _tglod;
+        quotaMintPerDate = _quotaMintPerDate;
+        quotaUserMintPerDate = _quotaUserMintPerDate;
+        verifyQuota = _verifyQuota;
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(SIGNER_ROLE, _msgSender());
+    }
+
+    function setTgold(ITGlod _tglod)
+        external
+        onlyAdmin
+        notNull(address(_tglod))
+    {
+        tglod = _tglod;
+    }
+
+    function addSigner(address signer) external onlyAdmin notNull(signer) {
+        grantRole(SIGNER_ROLE, signer);
+    }
+
+    function removeSigner(address signer) external onlyAdmin notNull(signer) {
+        revokeRole(SIGNER_ROLE, signer);
+    }
+
+    function setQuotaMintPerDate(uint256 amount) external onlyAdmin {
+        require(amount > 0, "RewardManagement: Amount Invalid");
+        quotaUserMintPerDate = amount;
+    }
+
+    function setQuotaUserMintPerDate(uint256 amount) external onlyAdmin {
+        require(amount > 0, "RewardManagement: Amount Invalid");
+        quotaUserMintPerDate = amount;
+    }
+
+    function setExpiredTime(uint256 expiredTime) external onlyAdmin {
+        require(expiredTime > 0, "RewardManagement: ExpiredTime Invalid");
+        _expiredTime = expiredTime;
+    }
+
+    function setVerifyQuota(bool status) external onlyAdmin {
+        verifyQuota = status;
+    }
+
+    function claim(
+        uint256 amount,
+        uint256 timestamp,
+        string memory claimId,
+        bytes calldata signature
+    ) external {
+        require(
+            (block.timestamp - timestamp) <= _expiredTime,
+            "RewardManagement: Transaction Expired"
+        );
+        require(!_claimId[claimId], "RewardManagement: Transaction Executed");
+
+        bytes32 hashMessage = keccak256(
+            abi.encodePacked(amount, timestamp, claimId)
+        );
+        bytes32 prefixed = hashMessage.prefixed();
+        address singer = prefixed.recoverSigner(signature);
+        require(
+            hasRole(SIGNER_ROLE, singer),
+            "RewardManagement: Signature Invalid"
+        );
+
+        uint32 date = _getCurrentDate();
+        if (verifyQuota) {
+            require(
+                _dateQuota[date] <= quotaMintPerDate,
+                "RewardManagement: Quota Per Date Exceed"
+            );
+
+            require(
+                _userQuota[_msgSender()][date] <= quotaUserMintPerDate,
+                "RewardManagement: Quota User Per Date Exceed"
+            );
+        }
+
+        _dateQuota[date] = _dateQuota[date].add(amount);
+        _userQuota[_msgSender()][date] = _dateQuota[date].add(amount);
+        _claimId[claimId] = true;
+        tglod.mint(_msgSender(), amount);
+        emit ClaimReward(_msgSender(), amount, claimId);
+    }
+
+    function _getCurrentDate() internal view returns (uint32) {
+        return uint32(block.timestamp / SECOND_PER_DATE);
+    }
+}
--- contracts/TGold.sol
+++ contracts/TGold.sol
@@ -2,29 +2,47 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract TGold is AccessControlEnumerable, ERC20Burnable {
-
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor(string memory name, string memory symbol)
-        ERC20(name, symbol)
-    {
+    modifier onlyAdmin() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "TGold: must have admin role"
+        );
+        _;
+    }
+
+    modifier onlyMinter() {
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "TGold: must have minter role"
+        );
+        _;
+    }
+
+    modifier notNull(address _address) {
+        require(_address != address(0), "TGold: address invalid");
+        _;
+    }
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         address msgSender = _msgSender();
-
         _setupRole(DEFAULT_ADMIN_ROLE, msgSender);
-
         _setupRole(MINTER_ROLE, msgSender);
     }
 
-    function mint(address to, uint256 amount)
-        public
-    {
-        require(hasRole(MINTER_ROLE, _msgSender()), "TGold: must have minter role to mint");
-
+    function mint(address to, uint256 amount) external onlyMinter {
         _mint(to, amount);
     }
 
+    function addMinter(address minter) external onlyAdmin notNull(minter) {
+        grantRole(MINTER_ROLE, minter);
+    }
+
+    function removeMinter(address minter) external onlyAdmin notNull(minter) {
+        revokeRole(MINTER_ROLE, minter);
+    }
 }
